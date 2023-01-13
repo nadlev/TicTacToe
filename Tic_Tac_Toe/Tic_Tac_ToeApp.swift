c0d6cb4a626acceb2024666d59354f13503bfb6d
nadlev <nadiya.levytska.jr@gmail.com>
2023-01-13 12:52:17 +0200

    Initial Commit

---
+++ Tic_Tac_Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic_Tac_Toe
+//
+//  Created by Надежда Левицкая on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
